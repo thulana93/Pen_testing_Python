3bb5c21d9f2df36259eddd0b120e298913d9638b
thulana <thulanavimukthi93@gmail.com>
2024-07-23 12:46:20 -0400

    check with regex

--- MAC_changer.py
+++ MAC_changer.py
@@ -1,5 +1,6 @@
 import subprocess
 import optparse
+import re
 
 parser = optparse.OptionParser()
 parser.add_option("--interface", dest="interface", help= "Interface to change MAC")
@@ -12,7 +13,17 @@ def main(interface, MAC):
     subprocess.call('sudo ifconfig ' + interface + ' down', shell=True)
     subprocess.call('sudo ifconfig ' + interface +  ' hw ether ' + MAC, shell=True)
     subprocess.call('sudo ifconfig ' + interface + ' up', shell=True)
-    subprocess.call('ifconfig ' + interface, shell=True)
-
+    results = subprocess.check_output(['ifconfig', interface])
+    #print(results)
+    results= results.decode('utf-8')
+    mac_out=re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w:", results)
+    if mac_out:
+        if MAC == mac_out:
+            print('[+] MAC Address is changed successfully')
+        else:
+            print('[-] MAC Address is not changed')
+    else:
+        print('[-] MAC Address not identified')
+    
 if __name__=="__main__":
     main(interface, MAC)
